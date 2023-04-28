10473b6c8119a94c1098d7faf8f8523c4b711514
Anibal Rivero <riveroleonel@gmail.com>
2023-04-28 20:56:57 -0300

    modifiacion

--- problema3.js
+++ problema3.js
@@ -1,11 +1,30 @@
-let numAdmitidoUno = prompt("Ingrese primer numero");
-let numAdmitidoDos = prompt("Ingrese segundo numero");
-let numAdmitidoTres = prompt("Ingrese tercer numero");
 
-if ((numAdmitidoUno == numAdmitidoDos) && (numAdmitidoUno == numAdmitidoTres)) {
-    document.write("Premio mayor : 500000 dolares");
-} else if ((numAdmitidoUno == numAdmitidoDos) || (numAdmitidoUno == numAdmitidoTres) || (numAdmitidoDos == numAdmitidoTres)) {
-    document.write("Premio segundo : 150000 dolares");
-} else {
-    document.write("Ganaste un helado");
+
+const numAdmitidoUno = document.querySelector("#numero-1");
+const numAdmitidoDos = document.querySelector("#numero-2");
+const numAdmitidoTres = document.querySelector("#numero-3");
+const resultadoPremio = document.querySelector("#premio");
+
+
+
+
+
+
+
+function confirmacionPremio() {
+
+    let num1 = numAdmitidoUno.value;
+    let num2 = numAdmitidoDos.value;
+    let num3 = numAdmitidoTres.value;
+
+
+
+
+    if ((num1 == num2) && (num1 == num3)) {
+        resultadoPremio.innerHTML = ("Premio mayor : 500000 dolares");
+    } else if ((num1 == num2) || (num1 == num3) || (num2 == num3)) {
+        resultadoPremio.innerHTML = ("Premio segundo : 150000 dolares");
+    } else {
+        resultadoPremio.innerHTML = ("Ganaste un helado");
+    }
 }
