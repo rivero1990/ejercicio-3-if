8b65432ebb78498f6284d5e95399cdce472ae659
Anibal Rivero <riveroleonel@gmail.com>
2023-04-29 15:11:28 -0300

    modifiacion

--- problema3.js
+++ problema3.js
@@ -1,14 +1,7 @@
-
-
 const numAdmitidoUno = document.querySelector("#numero-1");
 const numAdmitidoDos = document.querySelector("#numero-2");
 const numAdmitidoTres = document.querySelector("#numero-3");
 const resultadoPremio = document.querySelector("#premio");
-
-
-
-
-
 
 
 function confirmacionPremio() {
@@ -16,9 +9,6 @@ function confirmacionPremio() {
     let num1 = numAdmitidoUno.value;
     let num2 = numAdmitidoDos.value;
     let num3 = numAdmitidoTres.value;
-
-
-
 
     if ((num1 == num2) && (num1 == num3)) {
         resultadoPremio.innerHTML = ("Premio mayor : 500000 dolares");
